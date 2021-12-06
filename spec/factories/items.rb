1c722ba503e254a0af22cd2e55d4dadf09cc8537
Didi Dodson <mdodson55@gmail.com>
2021-12-06 16:54:46 -0700

    Add item files

---
+++ spec/factories/items.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :item, class: Item do
+    association :merchant
+
+    name { Faker::Company.name }
+    description { Faker::Lorem.paragraph }
+    unit_price { Faker::Number.within(range: 1..1000) }
+    id { Faker::Number.unique.within(range: 1..1000000) }
+  end
+end
