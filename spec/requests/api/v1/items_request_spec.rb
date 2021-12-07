c9c09006675cdf0f49c7e960da24ec57d9489f90
Didi Dodson <mdodson55@gmail.com>
2021-12-06 17:53:04 -0700

    Add item show and create

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -33,7 +33,6 @@ describe "Items API" do
 
   it "sends a list of items" do
     create_list(:item, 3)
-    merchant_id = create(:merchant).id
 
     get "/api/v1/items"
 
@@ -59,5 +58,69 @@ describe "Items API" do
       expect(item).to have_key(:merchant_id)
       expect(item[:merchant_id]).to be_a(Integer)
     end
+  end
+
+  it 'can get one item by id' do
+    item_id = create(:item).id
+
+    get "/api/v1/items/#{item_id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_an(Integer)
+
+    expect(item).to have_key(:name)
+    expect(item[:name]).to be_a(String)
+
+    expect(item).to have_key(:description)
+    expect(item[:description]).to be_a(String)
+
+    expect(item).to have_key(:unit_price)
+    expect(item[:unit_price]).to be_a(Float)
+
+    expect(item).to have_key(:merchant_id)
+    expect(item[:merchant_id]).to be_a(Integer)
+  end
+
+  it 'can create a new item' do
+    merch_id = create(:merchant).id
+
+    item_params = {
+                    name: 'Scratcher 3000',
+                    description: 'Top of the line scratcher for your feline friend',
+                    unit_price: 50.6,
+                    merchant_id: merch_id
+    }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+
+    created_item = Item.last
+
+    expect(response.status).to eq(201)
+    expect(response).to be_successful
+
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
+
+  it 'can update an item' do
+    id = create(:item).id
+    previous_name = Item.last.name
+    item_params = { title: 'Scratcher 6x' }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq('Scratcher 6x')
   end
 end
